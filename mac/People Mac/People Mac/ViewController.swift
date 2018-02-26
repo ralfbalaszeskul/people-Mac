e12ce01eecac2d7a129788f00cf424b4ed86ff69
Balaszeskul_Ralf <ralf.balaszeskul@solutions.endress.com>
2018-02-26 17:32:37 +0100

    Restructured Poject

---
+++ mac/People Mac/People Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  People Mac
+//
+//  Created by Ralf Balaszeskul on 26.02.18.
+//  Copyright © 2018 Ralf Balaszeskul. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ mac/People Mac/People MacTests/People_MacTests.swift
@@ -0,0 +1,36 @@
+//
+//  People_MacTests.swift
+//  People MacTests
+//
+//  Created by Ralf Balaszeskul on 26.02.18.
+//  Copyright © 2018 Ralf Balaszeskul. All rights reserved.
+//
+
+import XCTest
+@testable import People_Mac
+
+class People_MacTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
